12fa37ed9058b68600bbe7f1e06a654ba5adb66a
Logan Epps <logepps@gmail.com>
2023-01-25 19:52:10 -0600

    fixed img load on pagination

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,11 +1,11 @@
 const paginate = document.getElementById('paginate');
 const $campgroundsContainer = $('#campgrounds-container');
-paginate.addEventListener('click', function(e) {
+paginate.addEventListener('click', function (e) {
     e.preventDefault();
     fetch(this.href)
         .then(response => response.json())
         .then(data => {
-            for(const campground of data.docs) {
+            for (const campground of data.docs) {
                 let template = generateCampground(campground);
                 $campgroundsContainer.append(template);
             }
@@ -19,20 +19,20 @@ function generateCampground(campground) {
     let template = `<div class="card mb-3">
             <div class="row">
                 <div class="col-md-4">
-                    <img class="img-fluid" alt="" src="${ campground.images.length ? campground.images[0].url : 'https://res.cloudinary.com/dw0rkoozx/image/upload/v1625151568/YelpCamp/vtjiotlo4qjjzhvmjqbu.jpg' }">
+                    <img class="img-fluid" alt="" src="${campground.images.length ? campground.images[0].url : 'https://res.cloudinary.com/dw0rkoozx/image/upload/v1625151568/YelpCamp/vtjiotlo4qjjzhvmjqbu.jpg'}" crossorigin>
                 </div>
                 <div class="col-md-8">
                     <div class="card-body">
-                        <h5 class="card-title">${ campground.name }</h5>
+                        <h5 class="card-title">${campground.name}</h5>
                         <p class="card-text">
-                            <small class="text-muted">${ campground.location }</small>
+                            <small class="text-muted">${campground.location}</small>
                         </p>
-                        <p class="card-text text-truncate">${ campground.description }</p>
-                        <a href="/campgrounds/${ campground._id }" class="btn btn-primary">View ${ campground.name }</a>
+                        <p class="card-text text-truncate">${campground.description}</p>
+                        <a href="/campgrounds/${campground._id}" class="btn btn-primary">View ${campground.name}</a>
                     </div>
                 </div>
             </div>
         </div>
     </div>`;
-return template;
+    return template;
 }
